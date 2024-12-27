542c46133075ea5a2a7bf6c2b4d99e6c0439212e
TQCnoARosto <43849125+TQCnoARosto@users.noreply.github.com>
2024-12-27 11:36:50 +0100

    Added links in compilations to all .class files

--- Compile.bat
+++ Compile.bat
@@ -1,15 +1,27 @@
-set mylist=^
+set sharedCompilePaths=^
     ./Shared/src/*.java^
     ./Shared/src/DataStructures/*.java^
     ./Shared/src/DataStructures/Messages/*.java
+	
+set clientCompilePaths=^
+    ./Client/src/*.java
+	
+set serverCompilePaths=^
+    ./Server/src/*.java
 
-javac -d Build/Client ./Client/src/Main.java %mylist%
-javac -d Build/Server ./Server/src/Main.java %mylist%
+javac -d Build/Client %clientCompilePaths% %sharedCompilePaths%
+javac -d Build/Server %serverCompilePaths% %sharedCompilePaths%
 
+	
+set linkPaths=^
+    ./*.class^
+	./DataStructures/*.class^
+	./DataStructures/Messages/*.class
+	
 cd ./Build/Client
-jar cfe ../Client.jar Main *.class
+jar cfe ../Client.jar Main %linkPaths%
 cd ../Server
-jar cfe ../Server.jar Main *.class
+jar cfe ../Server.jar Main %linkPaths%
 cd ../../
 
 copy ".\Client\src\client.properties" ".\Build\client.properties"
